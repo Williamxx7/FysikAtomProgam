dba74a76800a8599247120c049faadf34965aae9
Williamxx7 <williamlarsen210@gmail.com>
2022-11-18 10:29:53 +0100

    CIRCLE SPIN

--- sketch.js
+++ sketch.js
@@ -1,17 +1,24 @@
+let r =  1;
+let x= 52
+
+
+
 function setup(){
-  createCanvas(800,800)
-  background(0,0,100)
+  createCanvas(1920,975)
+  
   angleMode(DEGREES)
   fill(0,0,100)
 
 }
 
 function draw(){
+  background(0,0,100)
   translate(width/2 , height/2)
   fill(255,0,0)
   noStroke()
   ellipse(0,0,70,70)
   noFill()
+  r++;
 
   stroke(255,0,0)
   ellipse(0,0,150,150)
@@ -19,14 +26,28 @@ function draw(){
   stroke(255,255,0)
   ellipse(0,0,250,250)
   
-  stroke(255,0,0)
+  stroke(0,255,0)
   ellipse(0,0,350,350)
   
-  stroke(255,0,0)
+  stroke(0,0,255)
   ellipse(0,0,450,450)
 
-  stroke(255,0,0)
+  stroke(255,0,255)
   ellipse(0,0,550,550)
 
+  rotate(r)
+  fill(255,255,255)
+  noStroke()
+  ellipse(x,x,25,25)
+
+  stroke(0)
+  strokeWeight(3)
+  line(49,49,55,55)
+
+  if(keyIsPressed === true){
+    x+=25
+  }else {
+    x+=0
+  }
 
 }
