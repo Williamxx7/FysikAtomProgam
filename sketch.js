cd07103261df2d90b41f1cd5776789f0b56a72f8
Williamxx7 <williamlarsen210@gmail.com>
2022-11-18 10:01:03 +0100

    Alle ringe lavet

--- sketch.js
+++ sketch.js
@@ -1,9 +1,32 @@
-function setup() {
-  createCanvas(400, 400);
+function setup(){
+  createCanvas(800,800)
+  background(0,0,100)
+  angleMode(DEGREES)
+  fill(0,0,100)
+
 }
 
-function draw() {
-  background(220);
-  print("u gay")
-  print("fuck dig")
+function draw(){
+  translate(width/2 , height/2)
+  fill(255,0,0)
+  noStroke()
+  ellipse(0,0,70,70)
+  noFill()
+
+  stroke(255,0,0)
+  ellipse(0,0,150,150)
+
+  stroke(255,255,0)
+  ellipse(0,0,250,250)
+  
+  stroke(255,0,0)
+  ellipse(0,0,350,350)
+  
+  stroke(255,0,0)
+  ellipse(0,0,450,450)
+
+  stroke(255,0,0)
+  ellipse(0,0,550,550)
+
+
 }
